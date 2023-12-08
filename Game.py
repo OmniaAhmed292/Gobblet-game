9ed316ec340cf2e8f46d9a1783fa511e26359b79
hanan saeed <hanansaeedbs12@gmail.com>
2023-12-08 19:31:03 +0200

    add (check_win & check 3 in same row) functions

--- Game.py
+++ Game.py
@@ -32,25 +32,97 @@ class Game:
       print("\n")
     print("\n")
 
-  def is_valid(self, player, to_grid: Postion, from_grid: Postion = None, from_pile: int = None) -> None:
-    if from_grid and from_pile:
-      raise Exception("you either play from the grid or your piles")
-    if not from_grid and not from_pile:
-      raise Exception("you should be at least play from the grid or your piles")
+  def is_valid(self, player_id, to_grid, from_grid=None, from_pile=None):
+      if from_grid and from_pile:
+          raise Exception("You can either play from the grid or your piles, not both.")
+      if not from_grid and not from_pile:
+          raise Exception("You should play from either the grid or your piles at least.")
 
-    if from_grid and self.grid[from_grid.x][from_grid.y].rocks and self.grid[to_grid.x][to_grid.y].rocks[-1].id != player:
-      raise Exception("you can not play from another player rocks")
+      if from_grid and self.grid[from_grid.x][from_grid.y].rocks and self.grid[to_grid.x][to_grid.y].rocks[
+          -1].id != player_id:
+          raise Exception("You cannot play from another player's rocks.")
 
-    if self.grid[to_grid.x][to_grid.y].rocks and self.grid[to_grid.x][to_grid.y].rocks[-1].id != player:
-      # TODO check case if 3 in row else
-      raise Exception("you can not play on another player rock unless he had 3 in a row")
+      if self.grid[to_grid.x][to_grid.y].rocks and self.grid[to_grid.x][to_grid.y].rocks[-1].id != player_id:
+          
+          cnt = 0
 
-  def do_turn(self, player, to_grid: Postion, from_grid: Postion = None, from_pile: int = None) -> None:
-    self.is_valid(player, to_grid, from_grid, from_pile)
+          # Check for a winning condition in the row
+          for i in range(4):
+              if self.grid[to_grid.x][i].rocks and self.grid[to_grid.x][i].rocks[-1].id == self.grid[to_grid.x][to_grid.y].rocks[-1].id:
+                  cnt += 1
+
+          if cnt == 3:
+              return
+
+          # Reset counter for column check
+          cnt = 0
+
+          # Check for a winning condition in the column
+          for i in range(4):
+              if self.grid[i][to_grid.y].rocks and self.grid[i][to_grid.y].rocks[-1].id == self.grid[to_grid.x][to_grid.y].rocks[-1].id:
+                  cnt += 1
+
+          if cnt == 3:
+              return
+
+          if (
+                  (to_grid.x + 2 < 4 and to_grid.y + 2 < 4
+                   and self.grid[to_grid.x + 1][to_grid.y + 1].rocks
+                   and self.grid[to_grid.x + 2][to_grid.y + 2].rocks
+                   and self.grid[to_grid.x + 1][to_grid.y + 1].rocks[-1].id != player_id
+                   and self.grid[to_grid.x + 2][to_grid.y + 2].rocks[-1].id != player_id)
+                  or
+                  (to_grid.x - 2 >= 0 and to_grid.y - 2 >= 0
+                   and self.grid[to_grid.x - 1][to_grid.y - 1].rocks
+                   and self.grid[to_grid.x - 2][to_grid.y - 2].rocks
+                   and self.grid[to_grid.x - 1][to_grid.y - 1].rocks[-1].id != player_id
+                   and self.grid[to_grid.x - 2][to_grid.y - 2].rocks[-1].id != player_id)
+                  or
+                  (to_grid.x + 2 < 4 and to_grid.y - 2 >= 0
+                   and self.grid[to_grid.x + 1][to_grid.y - 1].rocks
+                   and self.grid[to_grid.x + 2][to_grid.y - 2].rocks
+                   and self.grid[to_grid.x + 1][to_grid.y - 1].rocks[-1].id != player_id
+                   and self.grid[to_grid.x + 2][to_grid.y - 2].rocks[-1].id != player_id)
+                  or
+                  (to_grid.x - 2 >= 0 and to_grid.y + 2 < 4
+                   and self.grid[to_grid.x - 1][to_grid.y + 1].rocks
+                   and self.grid[to_grid.x - 2][to_grid.y + 2].rocks
+                   and self.grid[to_grid.x - 1][to_grid.y + 1].rocks[-1].id != player_id
+                   and self.grid[to_grid.x - 2][to_grid.y + 2].rocks[-1].id != player_id)
+          ):
+              return
+          else:
+              raise Exception("You cannot play on another player's rock unless they have 3 in a row")
+
+  def do_turn(self, player_id, to_grid: Postion, from_grid: Postion = None, from_pile: int = None) -> None:
+    self.is_valid(player_id, to_grid, from_grid, from_pile)
     if from_pile:
-      self.grid[to_grid.x][to_grid.y].push(self.player[player].piles[from_pile].pop())
+      self.grid[to_grid.x][to_grid.y].push(self.player[player_id].piles[from_pile].pop())
     elif from_grid:
       self.grid[to_grid.x][to_grid.y].push(self.grid[from_grid.x][from_grid.y].pop())
+
+  def check_win(self):
+      cnt = 0
+      for i in range(4):
+          for j in range(4):
+              if self.grid[i][j].rocks and self.grid[0][j].rocks and self.grid[i][j].rocks[-1].id == self.grid[0][j].rocks[-1].id:
+                  cnt += 1
+              if cnt == 4:
+                  return self.grid[0][j].rocks[-1].id
+      cnt = 0
+      for i in range(4):
+            for j in range(4):
+                if self.grid[j][i].rocks and self.grid[0][j].rocks and self.grid[j][i].rocks[-1].id == self.grid[0][j].rocks[-1].id:
+                    cnt += 1
+                if cnt == 4:
+                    return self.grid[i][0].rocks[-1].id
+
+      if all(self.grid[i][i].rocks and self.grid[i][i].rocks[-1].id == self.grid[0][0].rocks[-1].id for i in range(4)):
+          return self.grid[0][0].rocks[-1].id
+
+          # Check anti-diagonal
+      if all(self.grid[i][3 - i].rocks and self.grid[i][3 - i].rocks[-1].id == self.grid[0][3].rocks[-1].id for i in range(4)):
+          return self.grid[0][3].rocks[-1].id
 
 
 #     def __init__(self):
@@ -82,10 +154,7 @@ class Game:
 #         # Validate move based on rules
 #         # Return True if valid, False if invalid
 #
-#     def check_win(self, player):
-#         # Check if 4 pieces in a row
-#         # Return True if win, False otherwise
-#
+
 #     def get_possible_moves(self):
 #         # Return list of legal moves for current player
 #
